19d881033149bc0a531b793b2c110d860490f119
Youngchan Kim <ovekyc@gmail.com>
2016-09-17 02:31:09 +0900

    change foreign key

--- drawml.sql
+++ drawml.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `drawml`.`user` (
-  `id` MEDIUMINT NOT NULL AUTO_INCREMENT,
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
   `date_created` DATETIME NULL,
   `date_modified` DATETIME NULL,
   `user_id` VARCHAR(45) NOT NULL,
@@ -9,16 +9,16 @@ CREATE TABLE `drawml`.`user` (
   UNIQUE INDEX `user_id_name_UNIQUE` (`user_id` ASC));
 
 CREATE TABLE `drawml`.`experiment` (
-  `id` MEDIUMINT NOT NULL AUTO_INCREMENT,
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
   `date_created` DATETIME NULL,
   `date_modified` DATETIME NULL,
   `name` VARCHAR(45) NOT NULL,
-  `user_id` VARCHAR(45) NOT NULL,
+  `user_id` INTEGER NOT NULL,
   `xml` BLOB NULL,
   `drawing` BLOB NULL,
   `input` INT NULL,
   PRIMARY KEY (`user_id`, `name`),
-  FOREIGN KEY user(user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+  FOREIGN KEY user(user_id) REFERENCES user(id) ON DELETE CASCADE,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
   UNIQUE INDEX `user_id_name_UNIQUE` (`user_id`, `name` ASC));
 
@@ -28,8 +28,8 @@ CREATE TABLE `drawml`.`data` (
   `date_modified` DATETIME NULL,
   `name` VARCHAR(45) NOT NULL,
   `path` VARCHAR(45) NOT NULL,
-  `user_id` VARCHAR(45) NOT NULL,
+  `user_id` INTEGER NOT NULL,
   PRIMARY KEY (`user_id`, `name`),
-  FOREIGN KEY user(user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+  FOREIGN KEY user(user_id) REFERENCES user(id) ON DELETE CASCADE,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
   UNIQUE INDEX `user_id_name_UNIQUE` (`user_id`, `name` ASC));
