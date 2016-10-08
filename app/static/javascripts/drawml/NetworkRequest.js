3a5a635e18f72c4f0e6ad6f656865f3b9206c3cd
koohanmo <rngksah@naver.com>
2016-10-08 22:46:47 +0900

    get_data  List

--- app/static/javascripts/drawml/NetworkRequest.js
+++ app/static/javascripts/drawml/NetworkRequest.js
@@ -1,62 +1,17 @@
+
+var server = "http://210.118.74.55:5000"
+
 function get_data() {
     $.ajax({
-        url : 'http://localhost:5000/data/api',
+        url : server+'/data/api',
         type : 'GET',
         async : true,
         success : function(dataList){
-            console.log(dataList);
-            //data_user_group
-            //<div class="list-group-item data">나의 데이터 1</div>
-              //       "user_id": 1,
-              // "name": "file2",
-              // "path": "file_path",
-              // "date_modified": "2016-09-18 00:48:34",
-              // "date_created": "2016-09-18 00:45:38",
-              // "id": 7
+            //console.log(dataList);
             for(var x in dataList){
-                $('#data_user_group').appendChild('<div class="list-group-item data">'+'dataList[x].name'+'</div>');
+                $('#data_user_group').append('<div class="list-group-item data ui-draggable ui-draggable-handle">'+'dataList[x].name'+'</div>');
             }
-
+            $( init );
         }
     });
 }
-
-
-//experiments/<exp_id>	GET	get specific exp
-function get_data() {
-    $.ajax({
-        url : 'http://localhost:5000/data/api',
-        type : 'GET',
-        async : true,
-        success : function(dataList){
-            console.log(dataList);
-        }
-    });
-}
-//experiments/<exp_id>	PATCH	update exp
-function get_data() {
-    $.ajax({
-        url : 'http://localhost:5000/data/api',
-        type : 'GET',
-        async : true,
-        success : function(dataList){
-            console.log(dataList);
-        }
-    });
-}
-
-
-//experiments/<exp_id>	DELETE	delete exp
-function get_data() {
-    $.ajax({
-        url : 'http://localhost:5000/data/api',
-        type : 'GET',
-        async : true,
-        success : function(dataList){
-            console.log(dataList);
-        }
-    });
-}
-
-//experiments/<exp_id>stop	DELETE	stop exp
-//experiments/<exp_id>status	GET	get experiement status
