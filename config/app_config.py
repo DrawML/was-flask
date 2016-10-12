a97c09549a79f8d49c1633b5cdb5d96b2f05fc85
Taeguk Kwon <xornrbboy@gmail.com>
2016-10-13 01:20:13 +0900

    Add CLOUDDFS_DOMAIN to config.

--- config/app_config.py
+++ config/app_config.py
@@ -1,6 +1,6 @@
 # Statement for enabling the development environment
 DEBUG = True
-PORT = 5000
+PORT = 5001
 
 # Define the application directory
 import os
@@ -11,7 +11,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # SQLite for this example
 DB_PROTO    = 'mysql://'
 DB_USER     = 'root'
-DB_PWD      = '0000'
+DB_PWD      = 'ajtwlsdnfl12!'
 DB_SERVER   = 'localhost'
 DB_NAME     = 'drawml'
 DB_URI = DB_PROTO + DB_USER + ':' + DB_PWD + '@' + DB_SERVER + '/' + DB_NAME
@@ -43,9 +43,11 @@ UPLOAD_FOLDER = '/data'
 # Setup configure for distribution module
 MASTER_ADDR = 'tcp://210.118.74.56:16000'
 RESULT_ROUTER_PROTOCOL = 'tcp'
-RESULT_ROUTER_ADDR = '210.118.74.142'
+RESULT_ROUTER_ADDR = '210.118.74.55'
 RESULT_ROUTER_PORT = '15000'
 
 # Cloud distribute file system configuration
 CLOUDDFS_ADDR = '210.118.74.67'
 CLOUDDFS_PORT = 9602
+
+CLOUDDFS_DOMAIN = '210.118.74.67:9602'
