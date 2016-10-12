5711c1293707278f655372cbfd47bb10f9944e67
Taeguk Kwon <xornrbboy@gmail.com>
2016-10-13 01:22:59 +0900

    Fix a app.py to read config file.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!env/bin/python3.5
 from app import server
+from config.app_config import PORT, DEBUG
 
 app = server.app
-app.run(host='0.0.0.0', threaded=True, processes=1, use_reloader=False)
+app.run(host='0.0.0.0', threaded=True, processes=1, use_reloader=False, debug=DEBUG, port=PORT)
